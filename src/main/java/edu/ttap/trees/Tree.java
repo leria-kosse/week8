c39debef972e7911135e7cd20ef28c53ef65e45b
leria-kosse <leria.kosse@gmail.com>
2026-03-23 11:01:37 -0500

    Files added

---
+++ src/main/java/edu/ttap/trees/Tree.java
@@ -0,0 +1,147 @@
+package edu.ttap.trees;
+
+import java.util.List;
+
+/**
+ * A generic binary tree implementation.
+ */
+public class Tree<T> {
+    /**
+     * A node of the binary tree.
+     */
+    public static class Node<T> {
+        public T value;
+        public Node<T> left;
+        public Node<T> right;
+
+        /**
+         * @param value the value of the node
+         * @param left the left child of the node
+         * @param right the right child of the node
+         */
+        Node(T value, Node<T> left, Node<T> right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        /**
+         * @param value the value of the node
+         */
+        Node(T value) {
+            this(value, null, null);
+        }
+    }
+
+    ///// From the reading...
+
+    private Node<T> root;
+
+    /**
+     * Constructs a new, empty binary tree.
+     */
+    public Tree() {
+        this.root = null;
+    }
+
+    /**
+     * @return a sample binary tree for testing purposes
+     */
+    public static Tree<Integer> makeSampleTree() {
+        Tree<Integer> tree = new Tree<Integer>();
+        tree.root = new Node<>(
+            5,
+            new Node<>(2,
+                new Node<>(1),
+                new Node<>(3)
+            ),
+            new Node<>(8,
+                new Node<>(7,
+                    new Node<>(6),
+                    null),
+                new Node<>(9,
+                    null,
+                    new Node<>(10)))
+        );
+        return tree;
+    }
+
+
+    /**
+     * @param node the root of the tree 
+     * @return the number elements found in this tree rooted at node
+     */
+    private int sizeH(Node<T> node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return 1 + sizeH(node.left) + sizeH(node.right);
+        }
+    }
+
+    /** @return the number of elements in the tree */
+    public int size() {
+        return sizeH(root);
+    }
+
+    ///// Part 1: Contains
+
+    /**
+     * @param value the value to search for
+     * @return true iff the tree contains <code>value</code>
+     */
+    public boolean contains(T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Part 2: Traversals
+
+    /**
+     * @return the elements of this tree collected via an in-order traversal
+     */
+    public List<T> toListInorder() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return the elements of this tree collected via a pre-order traversal
+     */
+    public List<T> toListPreorder() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return the elements of this tree collected via a post-order traversal
+     */
+    public List<T> toListPostorder() {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Part 3: Stringifying Trees
+   
+    /**
+     * @return a string represent of this tree in the form, "[x1, ..., xk]."
+     * The order of the elements is left unspecified.
+     */
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Extra: Pretty Printing
+    
+    /**
+     * @return a string represent of this tree in bulleted list form.
+     */
+    public String toPrettyString() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * The main driver for this program
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nothing to do. 'Run' via the JUnit tests instead!");
+    }
+}
---
+++ src/test/java/edu/ttap/trees/TreeTest.java
@@ -0,0 +1,69 @@
+package edu.ttap.trees;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TreeTest {
+    @Test
+    public void emptyTreeSizeTest() {
+        Tree<Integer> tree = new Tree<Integer>();
+        assertEquals(0, tree.size());
+    }
+
+    @Test
+    public void sampleTreeSizeTest() {
+        assertEquals(9, Tree.makeSampleTree().size());
+    }
+
+    @Test
+    public void emptyTreeContainsTest() {
+        Tree<Integer> tree = new Tree<Integer>();
+        assertFalse(tree.contains(7));
+        assertFalse(tree.contains(4));
+    }
+
+    @Test
+    public void sampleTreeContainsTest() {
+        Tree<Integer> sampleTree = Tree.makeSampleTree();
+        assertTrue(sampleTree.contains(6));
+        assertFalse(sampleTree.contains(4));
+    }
+
+    @Test
+    public void basicToListInorderTest() {
+        // N.B., need to upcast the Integer[] array to avoid a ClassCastException that
+        // arises from downcasting the result of toArray to Integer[].
+        assertArrayEquals((Object[]) new Integer[] {1, 2, 3, 5, 6, 7, 8, 9, 10},
+                          Tree.makeSampleTree().toListInorder().toArray());
+    }
+
+    @Test
+    public void basicToListPreorderTest() {
+        assertArrayEquals((Object[]) new Integer[] {5, 2, 1, 3, 8, 7, 6, 9, 10},
+                          Tree.makeSampleTree().toListPreorder().toArray());
+    }
+
+    @Test
+    public void basicToListPostorderTest() {
+        assertArrayEquals((Object[]) new Integer[] {1, 3, 2, 6, 7, 10, 9, 8, 5},
+                          Tree.makeSampleTree().toListPostorder().toArray());
+    }
+
+    @Test
+    public void emptyTreeToStringTest() {
+        Tree<Integer> tree = new Tree<Integer>();
+        assertEquals("[]", tree.toString());
+    }
+
+    @Test
+    public void sampleTreeToStringTest() {
+        Tree<Integer> sampleTree = Tree.makeSampleTree();
+        assertEquals("TODO: fille me in with the intended results of your toString implementation",
+            sampleTree.toString());
+    }
+
+}
